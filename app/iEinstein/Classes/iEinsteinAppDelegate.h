b8bf75a7b4aa07dcdf0f5ddf62d87963bfa200d9
Matthias Melcher <mm@mathiasm.com>
2010-09-15 20:34:54 +0000

    Mostly Copyright updates.

--- app/iEinstein/Classes/iEinsteinAppDelegate.h
+++ app/iEinstein/Classes/iEinsteinAppDelegate.h
@@ -1,10 +1,25 @@
+// ==============================
+// File:			iEinsteinAppDelegate.h
+// Project:			Einstein
 //
-//  iEinsteinAppDelegate.h
-//  iEinstein
+// Copyright 2010 by Matthias Melcher (einstein@matthiasm.com).
 //
-//  Created by Matthias Melcher on 12.09.10.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 //
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// ==============================
+// $Id$
+// ==============================
 
 #import <UIKit/UIKit.h>
 
