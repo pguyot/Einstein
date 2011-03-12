58003b07f28e1268a3b5087a905471c80aea670d
Matthias Melcher <mm@mathiasm.com>
2011-03-12 22:58:25 +0000

    Android: working

--- _Build_/EclipseAndroid/README.txt
+++ _Build_/EclipseAndroid/README.txt
@@ -4,3 +4,5 @@ Compile the native code first using something like
   ~/dev/android-ndk-r5b/ndk-build
 
 from this directory
+
+  android update project -p . -s
--- _Build_/EclipseAndroid/src/com/example/einstein/einstein.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/einstein.java
@@ -27,23 +27,25 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
-import android.widget.TextView;
-import android.widget.LinearLayout;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.view.MotionEvent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 class EinsteinView extends View {
     private Bitmap mBitmap;
-
-    private static native void renderEinsteinView(Bitmap bitmap);
 
     public EinsteinView(Context context) {
         super(context);
@@ -55,83 +57,78 @@ class EinsteinView extends View {
     }
 
     @Override protected void onDraw(Canvas canvas) {
-        //canvas.drawColor(0xFFCCCCCC);
         renderEinsteinView(mBitmap);
-        canvas.drawBitmap(mBitmap, 0, 0, null);
-        // force a redraw, with a different time-based pattern.
-        invalidate();
+        Rect srcRect = new Rect(0, 0, 320, 480); 
+        Rect dstRect = new Rect(); getDrawingRect(dstRect);
+        canvas.drawBitmap(mBitmap, srcRect, dstRect, null);
     }
+    
+    @Override
+    public boolean onTouchEvent (MotionEvent ev) {
+    	// http://developer.android.com/reference/android/view/MotionEvent.html
+    	switch (ev.getAction()) {
+    	case MotionEvent.ACTION_DOWN:
+    	case MotionEvent.ACTION_MOVE:
+            Rect dstRect = new Rect(); getDrawingRect(dstRect);
+    		penDown((int)(ev.getX()*320/dstRect.width()), (int)(ev.getY()*480/dstRect.height()));
+    		break;
+    	case MotionEvent.ACTION_UP:
+    	case MotionEvent.ACTION_CANCEL:
+    		penUp();
+    		break;
+    	}
+    	return true;
+    }
+
+    // send refresh requests
+    private static native int renderEinsteinView(Bitmap bitmap);
+
+	// send touch events
+	public native void penDown(int x, int y);
+	public native void penUp();   
 }
 
 public class einstein extends Activity
 {
+	public EinsteinView pEinsteinView = null;
+	
+	public class ScreenRefresh extends TimerTask {
+
+		public EinsteinView pev = null;
+		
+		public ScreenRefresh(EinsteinView ev) {
+			pev = ev;
+		}
+		
+		@Override public void run() {
+			if (screenIsDirty()!=0)
+				pev.postInvalidate();
+		}
+	};
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        pEinsteinView = new EinsteinView(this);        
+        setContentView(pEinsteinView);
 
-        EinsteinView ev = new EinsteinView(this);
-        
-        TextView  tv = new TextView(this);
-        tv.setText( stringFromJNI() );
-        
-        LinearLayout grp = new LinearLayout(this);
-        grp.setOrientation(LinearLayout.VERTICAL);
-        grp.addView(tv);
-        grp.addView(ev);
-        
-        setContentView(grp);
-
-        /*
-        File dataDir = getExternalFilesDir("data");
-        AssetManager assetManager = getAssets();
-        InputStream inputStream = null;
-        FileOutputStream of = null;
-
-        int i;
-        String ff[] = dataDir.list(null);
-    	Log.e("tag", ">>>");
-        for (i=0; i<ff.length; i++) {
-        	Log.e("tag", ff[i]);
-        }
-    	Log.e("tag", "<<<");
-        
-        try {
-        	String str = dataDir.getAbsolutePath();
-        	inputStream = assetManager.open("717006.rom.png", AssetManager.ACCESS_RANDOM);
-        	of = new FileOutputStream(dataDir.getAbsolutePath()+"/717006.rom");
-        */	
-        	
+        String dataPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/Einstein"; 
+        if (!installAssets(dataPath))
+        	return;
 		initEmulator();
-		runEmulator(getExternalFilesDir("data").getAbsolutePath());
+		runEmulator(dataPath);
+		
+		Timer t = new Timer();
+		t.scheduleAtFixedRate(new ScreenRefresh(pEinsteinView), 1000, 200);
     }
 
     //@Override
     //protected void onStop() {
-    //    finish();
+    //    finish();  // FIXME: we must cancel the main thread
     //}
-	
-    @Override
-    public boolean onTouchEvent (MotionEvent ev) {
-    	// http://developer.android.com/reference/android/view/MotionEvent.html
-     final int historySize = ev.getHistorySize();
-     final int pointerCount = ev.getPointerCount();
-     for (int h = 0; h < historySize; h++) {
-         System.out.printf("At time %d:", ev.getHistoricalEventTime(h));
-         for (int p = 0; p < pointerCount; p++) {
-             System.out.printf("  pointer %d: (%f,%f)",
-                 ev.getPointerId(p), ev.getHistoricalX(p, h), ev.getHistoricalY(p, h));
-         }
-     }
-     System.out.printf("At time %d:", ev.getEventTime());
-     for (int p = 0; p < pointerCount; p++) {
-         System.out.printf("  pointer %d: (%f,%f)",
-             ev.getPointerId(p), ev.getX(p), ev.getY(p));
-     }
-       
-	return false;	
-    }
 
     // native test method implemented in app/AndroidGlue.c
     public native String  stringFromJNI();
@@ -141,6 +138,105 @@ public class einstein extends Activity
 	
 	// launch the emulator
 	public native void runEmulator(String dataPath);
+
+	// return 1 if the screen want to be re-rendered
+	public native int screenIsDirty();
+
+	public boolean installAssets(String dataPath) {
+        File dataDir = new File(dataPath);
+        dataDir.mkdirs();
+        AssetManager assetManager = getAssets();
+        
+        try {
+        	File rom = new File(dataPath + "/717006.rom");
+        	if (rom.exists()) {
+        		Log.i("Einstein", "ROM file found.");
+        	} else {
+        		InputStream in = null;
+        		try {
+        			in = assetManager.open("717006.rom.png");
+                } catch (IOException e) {
+            		finishWithMessage(
+            				"ROM file not found.\n\n" +
+            				"Please copy the files\n\n  \"717006.rom\" " +
+            				"and \"Einstein.rex\"\n\nto\n\n" +
+            				"  \"/Downloads/Einstein\"\n\non your Android device.");
+            		return false;
+        		}
+        		Log.w("Einstein", "Copying ROM file from assets...");
+        		FileOutputStream out = new FileOutputStream(dataPath+"/717006.rom");
+        		int sz = in.available();
+        		byte[] b = new byte[sz];
+        		in.read(b);
+        		out.write(b);
+        		in.close();
+        		out.close();
+        		Log.w("Einstein", "Done.");
+        	}
+        	File rex = new File(dataPath + "/Einstein.rex");
+        	if (rex.exists()) {
+        		Log.i("Einstein", "REX file found.");
+        	} else {
+        		InputStream in = null;
+        		try {
+        			in = assetManager.open("Einstein.rex.png");
+                } catch (IOException e) {
+            		finishWithMessage(
+            				"REX file not found.\n\n" +
+            				"Please copy the files\n\n  \"717006.rom\" " +
+            				"and \"Einstein.rex\"\n\nto\n\n" +
+            				"  \"/Downloads/Einstein\"\n\non your Android device.");
+            		return false;
+        		}
+                Log.w("Einstein", "Copying REX file from assets...");
+        		FileOutputStream out = new FileOutputStream(dataPath+"/Einstein.rex");
+        		int sz = in.available();
+        		byte[] b = new byte[sz];
+        		in.read(b);
+        		out.write(b);
+        		in.close();
+        		out.close();
+        		Log.w("Einstein", "Done.");
+        	}
+        	File img = new File(dataPath + "/717006.img");
+        	if (!img.exists()) {
+        		warnUser(
+        				"EXPERIMENTAL CODE\n\n" +
+        				"This is a very early prerelease of Einstein for Android. First boot may take up to six minutes! " +
+        				"Please watch for stray threads eating battery after Einstein quits!");
+        	} 
+        } catch (IOException e) {
+        	Log.e("Einstein", "Installing assets: " + e.getMessage());
+        }
+        return true;
+	}
+	
+	void finishWithMessage(String msg) {
+		class MyOnClickListener implements DialogInterface.OnClickListener {
+			Activity pv = null;
+			MyOnClickListener(Activity v) { pv = v; }
+			@Override public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+				pv.finish();
+			}
+		};
+		AlertDialog ad = new AlertDialog.Builder(this).create();  
+		ad.setCancelable(false); // This blocks the 'BACK' button  
+		ad.setMessage(msg);  
+		ad.setButton("Quit", new MyOnClickListener(this));  
+		ad.show();
+	}
+	
+	void warnUser(String msg) {
+		AlertDialog ad = new AlertDialog.Builder(this).create();  
+		ad.setCancelable(false); // This blocks the 'BACK' button  
+		ad.setMessage(msg);  
+		ad.setButton("OK", new DialogInterface.OnClickListener() { 
+			@Override public void onClick(DialogInterface dialog, int which) { dialog.dismiss(); } 
+			} 
+		);  
+		ad.show();
+	}
 	
     // load the entire native program as a library at startup
     static {
