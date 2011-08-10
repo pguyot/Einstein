6d4ff2c73dd3d111fa81635421e43249f23f402f
Steven Frank <stevenf@panic.com>
2011-08-10 04:28:41 +0000

    project modernization to Xcode 4.1 format and Mac OS X 10.6 base SDK

--- app/TCocoaMonitorController.h
+++ app/TCocoaMonitorController.h
@@ -11,7 +11,7 @@
 class TMacMonitor;
 
 
-@interface TCocoaMonitorController : NSWindowController 
+@interface TCocoaMonitorController : NSWindowController <NSTextFieldDelegate>
 {
 	IBOutlet NSTextField* commandField;
 	IBOutlet NSTextView* textView;
