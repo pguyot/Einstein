a455074b3a7c91b9278160a4c6ae0e74a75a9c63
Matthias Melcher <mm@mathiasm.com>
2011-11-25 19:49:38 +0000

    Fixed silent fail for missing native link. Added tick log to verify that tasks are actually finised when the app closes

--- _Build_/EclipseAndroid/src/com/example/einstein/ScreenRefresh.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/ScreenRefresh.java
@@ -2,21 +2,34 @@ package com.example.einstein;
 
 import java.util.TimerTask;
 
+import android.util.Log;
+
 class ScreenRefresh extends TimerTask {
 
+	public einstein pe = null;
 	public EinsteinView pev = null;
 	
-	public ScreenRefresh(EinsteinView ev) {
+	public ScreenRefresh(einstein e, EinsteinView ev) {
+		pe = e;
 		pev = ev;
 	}
 	
 	@Override
 	public void run() {
-		if (screenIsDirty()!=0)
+		cc++;
+		if (cc==10) {
+			cc = 0;
+			//Log.i("ScreenRefresh", "Tick");
+		}
+		if (pe.screenIsDirty()!=0) {
 			pev.postInvalidate();
+			//Log.i("ScreenRefresh", "Drawing");
+		}
+		
 	}
 	
 	// return 1 if the screen wants to be re-rendered
 	public native int screenIsDirty();
 
+	int cc = 0;
 }
