1d27073066689b36bf4a83b17b65d7843ce2e015
Frank Gründel <ickebindet1@mailinator.com>
2011-12-30 00:34:34 +0000

    #IM Added utility method showInfoDialog

--- _Build_/EclipseAndroid/src/com/example/einstein/DebugUtils.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/DebugUtils.java
@@ -1,5 +1,8 @@
 package com.example.einstein;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 public class DebugUtils {
@@ -10,6 +13,23 @@ public class DebugUtils {
 	
 	public static void debugTextOnScreen(einstein es, String text) {
 		Toast.makeText(es, text, Toast.LENGTH_LONG).show();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Log this exception to log file
+		}
+	}
+	
+	public static void showInfoDialog(Context context, String text) {
+		final AlertDialog dialog = new AlertDialog.Builder(context).create();  
+		dialog.setCancelable(false);  
+		dialog.setMessage(text);  
+		dialog.setButton("OK", new DialogInterface.OnClickListener() { 
+			@Override public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			} 
+		});  
+		dialog.show();
 	}
 
 }
