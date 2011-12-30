cde685acc440303be5f9db07d878ffde34b75e2b
Frank Gründel <ickebindet1@mailinator.com>
2011-12-30 23:07:16 +0000

    #DC Removed comments

--- _Build_/EclipseAndroid/src/com/example/einstein/einstein.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/einstein.java
@@ -1,94 +1,3 @@
-// ==============================
-// File:			einstein.java
-// Project:			Einstein
-//
-// Copyright 2011 by Matthias Melcher (einstein@matthiasm.com)
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-// ==============================
-// $Id$
-// ==============================
-
-// Suggestion for Android Menu and Dialog setup, strictly divided into 
-// one-shot actions and persistent settings (Preferences)
-
-// 1: Menu
-//  Maintenance >> 2
-//  Install Newton Packages >> 3
-//  Insert Network Card / Remove Network Card
-//  [ ] Backlight (Checkbox) 
-//  Preferences >> 4
-
-// 2: Maintenance
-//  Install NewtonOS ROM:
-//   Creating the Newton ROM file >> Internet Link, Browser
-//   From The Network >> 2.1
-//   From SD Card >> 2.2
-//   Via USB >> 2.3
-//   Via Kies Air >> 2.4 (Only show if Kies Air exists)
-//  Reset Newton:
-//   Soft Reset >> Dialog: "Are you sure, this will..."
-//   Power Reset >> Dialog: "Are you sure, this will..."
-//   Hard Reset >> Dialog: "Are you sure, this will..."
-//   Cold Boot >> Dialog: "Are you sure, this will..."
-
-// 2.1 Network ROM install
-//  Text Dialog, user can enter a URL (http, ftp, file...), OK, Cancel
-//  OK -> Progress Dialog -> Verify ROM -> Success Dialog / Error Dialog
-
-// 2.2 Newton ROM from SD Card
-//  File Browser to search for the ROM as a plain file or zip on the current media, OK, Cancel
-//  OK -> Verify ROM -> Success Dialog / Error Dialog
-
-// 2.3 Newton via USB
-//  Dialog Box "ROM file must be copied to /Downloads/Einstein ...", OK, Cancel
-//  Launch settings to make device a USB drive
-
-// 2.4 Newton via Kies Air
-//  Dialog Box "ROM file must be copied to /Downloads/Einstein ...", OK, Cancel
-//  Launch Kies Air
-
-// 3: Install Newton Packages
-//  Same mennu as (2), additionally we can provide and FTP setup to unna.org and others
-
-// 4: Preferences
-//  Screen Resolution
-//   Preset Screen Resolution >> 4.1
-//   Custom Resolution >> Enter two integers
-//  Emulator Settings
-//   RAM Size >> 4.1
-//   [ ] Keep Network Card plugged in (Checkbox)
-//  Host Settings
-//   Resource Path >> Text Dialog defaulting to "/Downloads/Einstein" (or whatever)
-//   Back button >> 4.2
-
-// 4.1 Screen Resolution
-//  ( ) Original MP: 320x480
-//  ( ) Host Resolution: 800x1010 (or whatever the resolution may be minus the status bar)
-//  ( ) 3/4 Host Resolution: 600x700 (or whatever the resolution may be minus the status bar)
-//  ( ) 1/2 Host Resolution: 400x505 (or whatever the resolution may be minus the status bar)
-//  ( ) 1/3 Host Resolution: 300x400 (or whatever the resolution may be minus the status bar)
-//  ( ) Custom Resolution
-
-// 4.2 Android Back Button
-//  ( ) Quit Einstein, Reboot Newton
-//  ( ) Pause Einstein, Newton Sleeps
-//  ( ) Newton Dot Button
-//  ( ) Newton Context Menu (not sure if I can implement that)
-
-
 package com.example.einstein;
 
 import java.util.Timer;
@@ -129,30 +38,7 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 	private SharedPreferences sharedPrefs;
 	private SharedPreferences.OnSharedPreferenceChangeListener sharedPrefsListener;
 
-	/* We need to override these:
-	 * 
-	 * Activity Starts:  onCreate() (cold start)
-	 *         visible:  onStart()  <-  onRestart()
-	 *      foreground:  onResume()
-	 *         running
-	 *       backgroud:  onPause()
-	 *       invisible:  onStop()
-	 *        shutdown:  onDestroy()
-	 *            kill?
-	 *            
-	 *        examples:
-	 *           launch  create->start->resume
-	 *    "Home" button  pause->stop
-	 *     launch again  start->resume
-	 *     "off" button  pause
-	 *      "on" button  resume
-	 *    "back" button  pause->stop->destroy 
-	 *     launch again  create->start->resume
-	 *  Android "sleep"  pause
-	 *             wake  resume  
-	 */
-
-	/** Called when the activity is first created. */
+	/** Called when the app is started for the first time. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -178,7 +64,7 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 		startScreenRefresh();	
 
 	}
-	
+
 	private void registerPreferenceChangeListener() {
 		this.sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 		this.sharedPrefs.registerOnSharedPreferenceChangeListener(this);
@@ -189,17 +75,12 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 		// The keys are defined in preferences.xml
-		// TODO We are currently getting exceptions when using the getInt or getBoolean methods of sharedPreferences.
-		// Apart from that, we are getting an exception when we click on the preferences menu after the Einstein emulator is started.
 		if ("screenwidth".equals(key)) {
 			DebugUtils.debugTextOnScreen(this, "screen width changed ");
-			//DebugUtils.debugTextOnScreen(this, "screen width changed to " + String.valueOf(sharedPreferences.getInt(key, -1)));
 		} else if ("screenheight".equals(key)) {
 			DebugUtils.debugTextOnScreen(this, "screen height changed ");
-			//DebugUtils.debugTextOnScreen(this, "screen height changed to " + String.valueOf(sharedPreferences.getInt(key, -1)));
 		} else if ("keepnetwrorkcardpluggedin".equals(key)) {
 			DebugUtils.debugTextOnScreen(this, "card setting changed ");
-			//DebugUtils.debugTextOnScreen(this, "Network card plugged in state changed to " + String.valueOf(sharedPreferences.getBoolean(key, false)));
 		} else if ("install_rom".equals(key)) {
 			final String installationType = sharedPreferences.getString("install_rom", "1");
 			DebugUtils.debugTextOnScreen(this, "ROM installation path changed to ".concat(installationType));
@@ -210,21 +91,7 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 	/** Used to create the menu. */
 	public boolean onCreateOptionsMenu(Menu menu) {
 		Log.e("XXXX", ">>>>>>>>>> onCreateOptionsMenu(Menu)");
-		/*
-		// now seems to be a good time to find out about the available screen space
-		// so we can update the menu accordingly (can we? at run time?)
-		DisplayMetrics displaymetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int ht = displaymetrics.heightPixels;
-        int wt = displaymetrics.widthPixels;
-		Log.i("XXXX", "Display metrics are " + String.valueOf(wt) + "," + String.valueOf(ht));		
-        Rect dstRect = new Rect();
-        pEinsteinView.getDrawingRect(dstRect);
-		Log.i("XXXX", "Drawing view is " + String.valueOf(dstRect.width()) + "," + String.valueOf(dstRect.height()));
-		// Galaxy Note is 800x1280, available are 800x1230 
-		 */
-
-        // Create the menu using the XML file
+		// Create the menu using the XML file
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.mainmenu, menu);
 		return true;
@@ -242,9 +109,6 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 			// Launch Preferences activity
 			final Intent intent = new Intent(einstein.this, EinsteinPreferencesActivity.class);
 			startActivity(intent);
-//			// Some feedback to the user
-//			Toast.makeText(einstein.this, "Enter your user credentials.",
-//				Toast.LENGTH_LONG).show();
 			break;
 		case R.id.backlight:
 			if (backlightIsOn()==1)
@@ -260,7 +124,7 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 			break;
 		}
 		return true;	}
-	
+
 	@Override
 	public void onStart()
 	{
@@ -306,17 +170,6 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 		stopEmulator();
 		stopScreenRefresh();	
 		super.onDestroy();
-		// TODO: FreeLibrary(...); or use Class ClassLoader
-		// TODO: handle the function if it was called to free resources, but not to finish the app
-		// if (!isFinishing()) { } 
-	}
-
-	@Override
-	public void onBackPressed()
-	{
-		super.onBackPressed();
-		// showDialog(DIALOG_REALLY_EXIT_ID);
-		// this.finish();
 	}
 
 	// native test method implemented in app/AndroidGlue.c
@@ -348,7 +201,7 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 
 	// check if backlight is on
 	public native int backlightIsOn();
-	
+
 	// install packages that are new in the Einstein directory
 	public native void installNewPackages();
 
@@ -366,23 +219,6 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 			this.mProgressDialog.setCancelable(false);
 			this.mProgressDialog.show();
 			return this.mProgressDialog;		
-			/*        case DIALOG_REALLY_EXIT_ID:
-            dialog = new AlertDialog.Builder(this).setMessage(
-                                "Are you sure you want to exit?")
-            .setCancelable(false)
-            .setPositiveButton("Yes",
-                    new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    TestMain.this.finish();
-                }
-            })
-            .setNegativeButton("No",
-                    new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                }
-            }).create();
-            break; */
 		default:
 			return null;
 		}
@@ -440,163 +276,10 @@ public class einstein extends Activity implements OnSharedPreferenceChangeListen
 		ad.setButton("Quit", new MyOnClickListener(this));  
 		ad.show();
 	}
-//
-//	void warnUser(String msg) {
-//		AlertDialog ad = new AlertDialog.Builder(this).create();  
-//		ad.setCancelable(false);  
-//		ad.setMessage(msg);  
-//		ad.setButton("OK", new DialogInterface.OnClickListener() { 
-//			@Override public void onClick(DialogInterface dialog, int which) { dialog.dismiss(); } 
-//		} 
-//		);  
-//		ad.show();
-//	}
 
 	// load the entire native program as a library at startup
 	static {
 		System.loadLibrary("einstein");
 	}
-
-/*
- * 
- * 
- * permissions:
- * 	
-Òandroid.permission.WRITE_EXTERNAL_STORAGEÓ
-Òandroid.permission.READ_EXTERNAL_STORAGEÓ
-<uses-permission android:name="android.permission.INTERNET" />
-
-
-main.xml:
-<?xml version="1.0" encoding="utf-8"?>
-<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-   android:orientation="vertical"
-   android:layout_width="fill_parent"
-   android:layout_height="fill_parent"
-   >
-<TextView  
-   android:layout_width="fill_parent" 
-   android:layout_height="wrap_content" 
-   android:text="@string/hello"
-   />
-<Button 
-    android:text="Start long running task.." 
-    android:id="@+id/startBtn" 
-    android:layout_width="fill_parent" 
-    android:layout_height="wrap_content">
-</Button>
-</LinearLayout>
- * 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-
-import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
-public class AndroAsync extends Activity {
-
-    public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
-    private Button startBtn;
-    private ProgressDialog mProgressDialog;
-
-    // Called when the activity is first created. 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        startBtn = (Button)findViewById(R.id.startBtn);
-        startBtn.setOnClickListener(new OnClickListener(){
-            public void onClick(View v) {
-                startDownload();
-            }
-        });
-    }
-
-    private void startDownload() {
-        String url = "http://farm1.static.flickr.com/114/298125983_0e4bf66782_b.jpg";
-        new DownloadFileAsync().execute(url);
-    }
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DIALOG_DOWNLOAD_PROGRESS:
-                mProgressDialog = new ProgressDialog(this);
-                mProgressDialog.setMessage("Downloading file..");
-                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                mProgressDialog.setCancelable(false);
-                mProgressDialog.show();
-                return mProgressDialog;
-            default:
-                return null;
-        }
-    }
-    class DownloadFileAsync extends AsyncTask<String, String, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            showDialog(DIALOG_DOWNLOAD_PROGRESS);
-        }
-
-        @Override
-        protected String doInBackground(String... aurl) {
-            int count;
-
-            try {
-                URL url = new URL(aurl[0]);
-                URLConnection conexion = url.openConnection();
-                conexion.connect();
-
-                int lenghtOfFile = conexion.getContentLength();
-                Log.d("ANDRO_ASYNC", "Lenght of file: " + lenghtOfFile);
-
-                InputStream input = new BufferedInputStream(url.openStream());
-                OutputStream output = new FileOutputStream("/sdcard/some_photo_from_gdansk_poland.jpg");
-
-                byte data[] = new byte[1024];
-
-                long total = 0;
-
-                while ((count = input.read(data)) != -1) {
-                    total += count;
-                    publishProgress(""+(int)((total*100)/lenghtOfFile));
-                    output.write(data, 0, count);
-                }
-
-                output.flush();
-                output.close();
-                input.close();
-            } catch (Exception e) {}
-            return null;
-
-        }
-        protected void onProgressUpdate(String... progress) {
-             Log.d("ANDRO_ASYNC",progress[0]);
-             mProgressDialog.setProgress(Integer.parseInt(progress[0]));
-        }
-
-        @Override
-        protected void onPostExecute(String unused) {
-            dismissDialog(DIALOG_DOWNLOAD_PROGRESS);
-        }
-    }
-}
-
- */
-
-// ============================================================================== //
-// I can't uninstall it, there seems to be some kind of 'Uninstall Shield'.       //
-// ============================================================================== //
 
 }
