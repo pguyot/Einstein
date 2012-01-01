e82ea90f096ad772cf174d63762e26cbfc81ebd0
Frank Gründel <ickebindet1@mailinator.com>
2012-01-01 23:33:10 +0000

    #IM Added usage of preset emulator screen sizes

--- _Build_/EclipseAndroid/src/com/example/einstein/startup/Startup.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/startup/Startup.java
@@ -9,9 +9,14 @@ import com.example.einstein.R;
 import com.example.einstein.constants.StringConstants;
 import com.example.einstein.startup.IStartup.LoadResult;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 
 public class Startup {
 
@@ -50,6 +55,42 @@ public class Startup {
 			return LoadResult.APPLICATION_ICON_MISSING;
 		}
 		return LoadResult.OK;
+	}
+	
+	/** Returns the dimension of the Newton emulator screen. This depends on the preferences menu setting. We return an instance 
+	 * of class <code>Point</code> because surprisingly there is no <code>Dimension</code> class in the Android VM. */
+	public Point getNewtonScreenSize() {
+		// Get host screen dimensions. Note that this depends on the current screen rotation
+		// Why the heck doesn't getMetrics return a DisplayMetrics object??
+		final DisplayMetrics metrics = new DisplayMetrics();
+		final Activity activity = (Activity)context;
+		activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		int hostScreenWidth = metrics.widthPixels;
+		int hostScreenHeight = metrics.heightPixels;
+		DebugUtils.showInfoDialog(activity, "Host screen size is " + String.valueOf(hostScreenWidth) + " x " + String.valueOf(hostScreenHeight));
+		// Get current preferences setting
+		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		final String currentPreset = prefs.getString("screenpresets", "1");
+		// Calculate Newton screen dimensions depending on preferences setting
+		final boolean isPortrait = hostScreenWidth < hostScreenHeight;
+		int w = isPortrait ? 320 : 480;
+		int h = isPortrait ? 480 : 320;
+		if ("2".equals(currentPreset)) { // Host screen resolution
+			w = hostScreenWidth;
+			h = hostScreenHeight;
+		} else if ("3".equals(currentPreset)) { // 3/4 host screen resolution
+			w = hostScreenWidth * 3 / 4;
+			h = hostScreenHeight * 3 / 4;
+		} else if ("4".equals(currentPreset)) { // 1/2 host screen resolution
+			w = hostScreenWidth / 2;
+			h = hostScreenHeight / 2;
+		}
+		else if ("5".equals(currentPreset)) { // 1/3 host screen resolution
+			w = hostScreenWidth / 3;
+			h = hostScreenHeight / 3;
+		}
+		DebugUtils.showInfoDialog(activity, "Newton screen size is " + String.valueOf(w) + " x " + String.valueOf(h));
+		return new Point(w, h);
 	}
 
 	/** Returns <code>true</code> if a ROM file was found in <code>dataDir</code> */
