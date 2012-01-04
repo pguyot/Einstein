5de2c3b7dc5fb9ee4392bb93a643cea196c7ca3c
Frank Gründel <ickebindet1@mailinator.com>
2012-01-04 00:47:23 +0000

    #DC Removed FILE_SEPARATOR since this value is available from the File class. #IM Added log folder and log file names.

--- _Build_/EclipseAndroid/src/com/example/einstein/startup/StartupConstants.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/startup/StartupConstants.java
@@ -9,9 +9,6 @@ public class StartupConstants {
 	/** Standard directory in which to place files that have been downloaded by the user */
 	public static final File EXTERNAL_STORAGE_PUBLIC_DIR;
 	
-	/** Platform-dependent file path separator character */
-	public static final String FILE_SEPARATOR;
-	
 	/** The sub-directory in which the Newton emulator expects its files */
 	public static final String APPLICATION_SUBDIR;
 	
@@ -27,14 +24,21 @@ public class StartupConstants {
 	/** The name of the application icon image file */
 	public static final String APP_ICON_FILE_NAME;
 	
+	/** The folder in the SD card in which to put the log file */
+	public static final String LOG_FOLDER;
+	
+	/** The name of the log file */
+	public static final String LOG_FILE_NAME;
+	
 	static {
 		EXTERNAL_STORAGE_PUBLIC_DIR = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-		FILE_SEPARATOR = System.getProperty("file.separator");
 		APPLICATION_SUBDIR = "Einstein"; // TODO Change this?
 		ROM_FILE_NAME = "717006.rom";
 		REX_FILE_NAME = "Einstein.rex";
-		DATA_FILE_PATH = EXTERNAL_STORAGE_PUBLIC_DIR + FILE_SEPARATOR + APPLICATION_SUBDIR;
+		DATA_FILE_PATH = EXTERNAL_STORAGE_PUBLIC_DIR + File.separator + APPLICATION_SUBDIR;
 		APP_ICON_FILE_NAME = "717006.img";
+		LOG_FOLDER = "log";
+		LOG_FILE_NAME = "noselog.txt";
 	}
 
 }
