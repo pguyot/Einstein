7214f2fef8792793eb8759e95aab5f5a977ccc01
Frank Gründel <ickebindet1@mailinator.com>
2012-01-04 00:44:20 +0000

    #IM Added method to log to file. The file name is noselog.txt, and it is in Download\Einstein\log.

--- _Build_/EclipseAndroid/src/com/example/einstein/DebugUtils.java
+++ _Build_/EclipseAndroid/src/com/example/einstein/DebugUtils.java
@@ -1,16 +1,25 @@
 package com.example.einstein;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.widget.Toast;
 
+import com.example.einstein.startup.StartupConstants;
+
 public class DebugUtils {
-	
+
 	private DebugUtils() {
 		// No instances, please
 	}
-	
+
 	public static void debugTextOnScreen(einstein es, String text) {
 		Toast.makeText(es, text, Toast.LENGTH_LONG).show();
 		try {
@@ -19,7 +28,7 @@ public class DebugUtils {
 			// TODO Log this exception to log file
 		}
 	}
-	
+
 	public static void showInfoDialog(Context context, String text) {
 		final AlertDialog dialog = new AlertDialog.Builder(context).create();  
 		dialog.setCancelable(false);  
@@ -30,6 +39,61 @@ public class DebugUtils {
 			} 
 		});  
 		dialog.show();
+	}
+
+	/** Appends <code>text</code> to the log file. */
+	public static void appendLog(String text) {
+		final File logFile = getLogFile();
+		if (null == logFile) {
+			return;
+		}
+		BufferedWriter bufferedWriter = null;
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(logFile, true));
+			bufferedWriter.append(new Date().toGMTString() + ": " + text);
+			bufferedWriter.append("\r\n"); // Add line feed. Do not use buf.newLine() since it'll only add 0A, which doesn't help under Windows
+			bufferedWriter.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (null != bufferedWriter) {
+					bufferedWriter.close();
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/** Creates the log file unless it already exists. The log file'noselog.txt will be in the Download\Einstein folder.*/
+	private static File getLogFile() {
+		final String logFolderName = StartupConstants.DATA_FILE_PATH + File.separator + StartupConstants.LOG_FOLDER;
+		final File logFolder = new File(logFolderName);
+		if (!logFolder.exists()) {
+			final boolean success = logFolder.mkdir();
+			if (!success) {
+				return null;
+			}
+		}
+		final String logFileName = logFolderName + File.separator + StartupConstants.LOG_FILE_NAME;
+		final File logFile = new File(logFileName);
+		if (!logFile.exists()) {
+			try {
+				final boolean success = logFile.createNewFile();
+				if (!success) {
+					return null;
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return logFile;
 	}
 
 }
