682f3e56a08ba26cedf9aaeaf304536ea5844c65
Eckhart Köppen <eck@40hz.org>
2013-01-11 20:03:02 +0000

    Revert accidental change to using "run" as the default startup action and change it back to "revert"

--- app/TCocoaAppController.mm
+++ app/TCocoaAppController.mm
@@ -430,8 +430,8 @@ static TCocoaAppController* gInstance = nil;
 	// Start the thread.
 	[NSThread detachNewThreadSelector:@selector(runEmulator) toTarget: self withObject: NULL];
 //	[mMonitorController performSelector:@selector(executeCommand:) withObject:@"load x" afterDelay:1];
-	[mMonitorController performSelector:@selector(executeCommand:) withObject:@"run" afterDelay:1];
-//	[mMonitorController performSelector:@selector(executeCommand:) withObject:@"revert" afterDelay:1];
+//	[mMonitorController performSelector:@selector(executeCommand:) withObject:@"run" afterDelay:1];
+	[mMonitorController performSelector:@selector(executeCommand:) withObject:@"revert" afterDelay:1];
 }
 
 
