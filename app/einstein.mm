c6345a43517e40b2f21ca863fcba403a5aeeb86e
Steven Frank <stevenf@panic.com>
2013-02-04 22:03:28 +0000

    what was i thinking? you don't need a release pool here

--- app/einstein.mm
+++ app/einstein.mm
@@ -33,10 +33,7 @@
 int
 main(int argc, const char *argv[])
 {
-	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-	int ret = NSApplicationMain(argc, argv);
-	[pool release];
-	return ret;
+	return NSApplicationMain(argc, argv);
 }
 
 
