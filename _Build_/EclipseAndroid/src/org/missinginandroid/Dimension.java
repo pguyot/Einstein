e1436b9ed41f434254278c5b76ef90df51cd8bc0
Frank Gründel <ickebindet1@mailinator.com>
2013-08-05 22:07:32 +0000

    #BF Frank Gruendel Moved class from com.missinginandroid to package org.missinginandroid because the Einstein name space was changed significantly and the com package name should be removed altogether.

---
+++ _Build_/EclipseAndroid/src/org/missinginandroid/Dimension.java
@@ -0,0 +1,23 @@
+/** A Dimension class that encapsulates width and height values. This class is here because Android
+ * surprisingly does not know the java.awt.Dimension class. */
+
+package org.missinginandroid;
+ 
+public class Dimension {
+	
+	/** The width. 0 by default. */
+	public int width = 0;
+	
+	/** The height. 0 by default. */
+	public int height = 0;
+	
+	public Dimension(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public String toString() {
+		return ("Dimension (width = " + width + " height = " + height + ")");
+	}
+
+}
