968f856dc9c88306f85c7bf9f0cf1d1f644defa9
Frank Gründel <ickebindet1@mailinator.com>
2013-08-21 23:20:32 +0000

    #IM Frank Gruendel Removed unused class URLConstants.java

--- _Build_/EclipseAndroid/src/org/messagepademu/einstein/constants/URLConstants.java
+++
@@ -1,8 +0,0 @@
-// TODO FG Review
-
-package org.messagepademu.einstein.constants;
-
-public class URLConstants {
-	
-    public static final String SHIP_IMAGE = "http://farm1.static.flickr.com/114/298125983_0e4bf66782_b.jpg";
-}
