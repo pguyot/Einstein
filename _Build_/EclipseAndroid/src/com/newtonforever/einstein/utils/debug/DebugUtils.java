b31c406ddb84f122f753de131bb7a401eb195b6e
Frank Gründel <ickebindet1@mailinator.com>
2013-09-20 23:44:47 +0000

    #BF FG Since mkdir does create missing folders above the folder we want to create, we have to create folders step by step. Otherwise we'd get an exception when we start Einstein for the first time and the parent folder of the folder we want to create isn't yet there. #DC FG Using Environment.getExternalStorageDirectory().getPath() instead of hardcoding the path to the external storage directory

--- _Build_/EclipseAndroid/src/com/newtonforever/einstein/utils/debug/DebugUtils.java
+++ _Build_/EclipseAndroid/src/com/newtonforever/einstein/utils/debug/DebugUtils.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Environment;
 import android.widget.Toast;
 
 import com.newtonforever.einstein.startup.StartupConstants;
@@ -16,85 +17,101 @@ import com.newtonforever.einstein.startup.StartupConstants;
 /** A small halper class for debugging the Einstein app. @author Frank Gruendel */
 public class DebugUtils {
 
-	private DebugUtils() {
-		// No instances, please
-	}
+    private DebugUtils() {
+        // No instances, please
+    }
 
-	private static File logFile = null;
-	
-	/** Show a quick message and pause execution for a short moment. */
-	public static void debugTextOnScreen(final Context context, final String text) {
-		Toast.makeText(context, text, Toast.LENGTH_LONG).show();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			appendLog(e.getMessage());
-		}
-	}
+    private static File logFile = null;
 
-	/** Shows a message in a dialog and waits for the dialog to be dismissed. */
-	public static void showInfoDialog(final Context context, final String text) {
-		final AlertDialog dialog = new AlertDialog.Builder(context).create();  
-		dialog.setCancelable(false);  
-		dialog.setMessage(text);  
-		dialog.setButton("OK", new DialogInterface.OnClickListener() { 
-			@Override 
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-			} 
-		});  
-		dialog.show();
-	}
+    /** Show a quick message and pause execution for a short moment. */
+    public static void debugTextOnScreen(final Context context, final String text) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            appendLog(e.getMessage());
+        }
+    }
 
-	/** Appends <code>text</code> to the end of the log file. */
-	public static void appendLog(final String text) {
-		if (null == logFile) {
-			logFile = getLogFile();
-		}
-		BufferedWriter bufferedWriter = null;
-		try {
-			bufferedWriter = new BufferedWriter(new FileWriter(logFile, true));
-			bufferedWriter.append(new Date().toGMTString() + ": " + text);
-			bufferedWriter.append("\r\n"); // Add line feed. Do not use buf.newLine() since it'll only add hex 0A, which doesn't help under Windows
-			bufferedWriter.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				if (null != bufferedWriter) {
-					bufferedWriter.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	/** Creates and returns an empty text file for log output. This file can be found in the folder Download\Einstein. */
-	private static File getLogFile() {
-		if (null != logFile) {
-			return logFile;
-		}
-		final String logFolderName = StartupConstants.DATA_FILE_PATH + File.separator + StartupConstants.LOG_FOLDER;
-		final File logFolder = new File(logFolderName);
-		if (!logFolder.exists()) {
-			logFolder.mkdir();
-		}
-		final String logFileName = logFolderName + File.separator + StartupConstants.LOG_FILE_NAME;
-		logFile = new File(logFileName);
-		if (logFile.exists()) {
-			logFile.delete();
-		}
-		try {
-			logFile.createNewFile();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-		return logFile;
-	}
+    /** Shows a message in a dialog and waits for the dialog to be dismissed. */
+    public static void showInfoDialog(final Context context, final String text) {
+        final AlertDialog dialog = new AlertDialog.Builder(context).create();  
+        dialog.setCancelable(false);  
+        dialog.setMessage(text);  
+        dialog.setButton("OK", new DialogInterface.OnClickListener() { 
+            @Override 
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            } 
+        });  
+        dialog.show();
+    }
+
+    /** Appends <code>text</code> to the end of the log file. */
+    public static void appendLog(final String text) {
+        if (null == logFile) {
+            logFile = getLogFile();
+        }
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(logFile, true));
+            bufferedWriter.append(new Date().toGMTString() + ": " + text);
+            bufferedWriter.append("\r\n"); // Add line feed. Do not use buf.newLine() since it'll only add hex 0A, which doesn't help under Windows
+            bufferedWriter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (null != bufferedWriter) {
+                    bufferedWriter.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /** Creates and returns an empty text file for log output. This file can be found in the folder Download\Einstein. */
+    private static File getLogFile() {
+        if (null != logFile) {
+            return logFile;
+        }
+        final String logFolderName = StartupConstants.DATA_FILE_PATH + File.separator + StartupConstants.LOG_FOLDER;
+        final File logFolder = new File(logFolderName);
+        if (!logFolder.exists()) {
+            createLogFolderPath();
+        }
+        final String logFileName = logFolderName + File.separator + StartupConstants.LOG_FILE_NAME;
+        logFile = new File(logFileName);
+        if (logFile.exists()) {
+            logFile.delete();
+        }
+        try {
+            logFile.createNewFile();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return logFile;
+    }
+
+    private static void createLogFolderPath() {
+        final String externalStorageRoot = Environment.getExternalStorageDirectory().getPath();
+        File f = new File(externalStorageRoot + "/Download");
+        if (!f.exists()) {
+            f.mkdir();
+        }
+        f = new File(externalStorageRoot + "/Download/Einstein");
+        if (!f.exists()) {
+            f.mkdir();
+        }
+        f = new File(externalStorageRoot + "Download/Einstein/log");
+        if (!f.exists()) {
+            f.mkdir();
+        }
+    }
 }
