92a86851b3b677aafb4c772b8115bb2c64953c3e
Frank Gründel <ickebindet1@mailinator.com>
2013-10-23 22:04:40 +0000

    #IM Frank Gruendel Moved native calls required for playing sounds from this class to EinsteinActionHandler class. #IM Some refactoring to make code shorter.

--- _Build_/EclipseAndroid/src/com/newtonforever/einstein/sound/SoundManager.java
+++ _Build_/EclipseAndroid/src/com/newtonforever/einstein/sound/SoundManager.java
@@ -7,57 +7,51 @@ import android.media.ToneGenerator;
 import android.util.Log;
 
 import com.newtonforever.einstein.jni.Native;
+import com.newtonforever.einstein.startup.StartupConstants;
+import com.newtonforever.einstein.utils.debug.DebugUtils;
 
 public class SoundManager {
 
-    private final ToneGenerator m_toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-   
+    private final ToneGenerator m_toneGenerator;
+
+    private final AudioTrack m_audioTrack;
+
     public SoundManager() {
         super();
+        this.m_toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        final int streamType = AudioManager.STREAM_MUSIC;
+        final int sampleRateInHz = 22050;
+        final int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+        final int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        final int minBufferSize = StartupConstants.AUDIO_TRACK_MIN_BUFFER_SIZE;
+        final int bufferMode = AudioTrack.MODE_STREAM;
+        this.m_audioTrack = new AudioTrack(streamType, sampleRateInHz, channelConfig, audioFormat, minBufferSize, bufferMode);
+        this.m_audioTrack.play(); // Start playing the audio track        
     }
 
-    public void playSound() {
-        final int bufferSize = Native.getSoundBufferSize();
-        Log.e("SoundManager.playSound", "BufferSize is " + bufferSize);
-        final short[] soundBuffer = new short[bufferSize];
-        int bytesReceived = Native.fillSoundBuffer(soundBuffer);
-        Log.e("SoundManager.playSound", "Received " + bytesReceived + " bytes");
-        // TODO FG For now we will only play a short beep
-        //this.m_toneGenerator.startTone(ToneGenerator.TONE_DTMF_0, 100);
-        
-        if (audioTrack==null) {
-        	deviceBufferSize = android.media.AudioTrack.getMinBufferSize(
-                    22050, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT);
-            audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                    22050, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT, deviceBufferSize,
-                    AudioTrack.MODE_STREAM);
-            audioTrack.play();
+    public void playSound(final int soundBufferSize) {
+        DebugUtils.logGreen("SoundManager: ", "Entering playSound()");
+        DebugUtils.logGreen("SoundManager: ", "Sound buffer size: " + soundBufferSize);
+        if (0 == soundBufferSize) {
+            return;
         }
-        int samplesRemaining = bytesReceived/2;
-        int samplesStart = 0;
-        int bytesInBuffer = 0;
-        while (samplesRemaining>0) {
-        	int samplesWritten = audioTrack.write(soundBuffer, samplesStart, samplesRemaining);
-        	bytesInBuffer += 2*samplesWritten;
-        	Log.e("SoundManager.playSound", "Wrote " + samplesWritten + " bytes");
-        	samplesStart += samplesWritten;
-        	samplesRemaining -= samplesWritten;
-        }
+        final short[] soundBuffer = new short[soundBufferSize];
+        final int bytesReceived = Native.fillSoundBuffer(soundBuffer);
+        final int samplesReceived = bytesReceived / 2;
+        final int samplesWritten = m_audioTrack.write(soundBuffer, 0, samplesReceived);
+        final int bytesWritten = 2 * samplesWritten;
         // AudioTrack does not start playing a sound until the buffer is half full. This is a hack to flush the buffer.
-        if (bytesInBuffer<deviceBufferSize) {
-        	int fill = deviceBufferSize - bytesInBuffer;
-        	final short[] tmpBuffer = new short[fill/2];
-        	audioTrack.write(tmpBuffer, 0, fill/2);
-        	audioTrack.flush();
+        if (bytesWritten < StartupConstants.AUDIO_TRACK_MIN_BUFFER_SIZE) {
+            final int tmpBufferSize = (StartupConstants.AUDIO_TRACK_MIN_BUFFER_SIZE - bytesWritten) / 2;
+            final short[] tmpBuffer = new short[tmpBufferSize];
+            m_audioTrack.write(tmpBuffer, 0, tmpBufferSize);
+            m_audioTrack.flush();
         }
-        
-        Native.soundBufferFinishedOrCanceled();
+        DebugUtils.logGreen("SoundManager: ", "Leaving playSound()");
     }
 
     public void stopSound() {
-        Log.e("SoundManager.stopSound", "");
+        DebugUtils.logRed("SoundManager: ", "Entering stopSound()");                
         this.m_toneGenerator.stopTone();
     }
 
@@ -65,8 +59,5 @@ public class SoundManager {
         final int volume = Native.getSoundVolume();
         Log.e("SoundManager.changeVolume", "Changed volume to " + volume);
     }
-
-    AudioTrack audioTrack = null;
-    int deviceBufferSize = 0;
 }
 
