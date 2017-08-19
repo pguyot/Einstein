5f8d33e6581eb4f49e20d9e71720b28ca72687cc
Steven Frank <stevenf@panic.com>
2017-08-18 19:46:32 -0700

    history buffer now adapts to window resize

--- Monitor/TMacMonitorView.mm
+++ Monitor/TMacMonitorView.mm
@@ -10,8 +10,9 @@
 #import "TMacMonitorView.h"
 #import "TMacMonitor.h"
 
-const int historyVisible = 32;
-const int historySize = 1024;
+#define FONT_HEIGHT 13
+#define FONT_WIDTH 7
+#define HISTORY_SIZE 1024
 
 @interface TMacMonitorView ()
 {
@@ -33,8 +34,9 @@ const int historySize = 1024;
 	NSString *ic3;
 	NSString *symbol;
 	NSAttributedString *disasm[5];
-	NSAttributedString *history[historySize];
+	NSAttributedString *history[HISTORY_SIZE];
 	CGFloat historyOffset;
+	NSInteger historyVisible;
 	NSMutableString *command;
 
 	TCocoaMonitorController *controller;
@@ -51,9 +53,6 @@ const int historySize = 1024;
 	NSDictionary *errorAttrs;
 }
 @end
-
-#define FONT_HEIGHT 13
-#define FONT_WIDTH 7
 
 
 @implementation TMacMonitorView
@@ -145,8 +144,13 @@ const int historySize = 1024;
 #endif
 
 		command = [[NSMutableString alloc] init];
-
-		historyOffset = (CGFloat)(historySize - historyVisible);
+		
+		historyOffset = 0;
+		
+		for ( int i = 0; i < HISTORY_SIZE; ++i )
+		{
+			history[i] = NULL;
+		}
 	}
 
 	return self;
@@ -162,13 +166,17 @@ const int historySize = 1024;
 - (void)addHistoryLine:(NSString *)line type:(int)type
 {
 	// Reset scrolling history to bottom line
-	historyOffset = (CGFloat)(historySize - historyVisible);
+	
+	NSRect frame = [self frame];
+	historyVisible = (frame.size.height - (FONT_HEIGHT * 9)) / FONT_HEIGHT;
+	
+	historyOffset = (CGFloat)(HISTORY_SIZE - historyVisible);
 
 	// Scroll up
 #if !__has_feature(objc_arc)
 	[history[0] release];
 #endif
-	for ( int i = 1; i < historySize; ++i )
+	for ( int i = 1; i < HISTORY_SIZE; ++i )
 	{
 		history[i - 1] = history[i];
 	}
@@ -177,19 +185,19 @@ const int historySize = 1024;
 
 	if ( type == MONITOR_LOG_ERROR )
 	{
-		history[historySize-1] = [[NSAttributedString alloc] initWithString:line attributes:errorAttrs];
+		history[HISTORY_SIZE - 1] = [[NSAttributedString alloc] initWithString:line attributes:errorAttrs];
 	}
 	else if ( type == MONITOR_LOG_CODE )
 	{
-		history[historySize-1] = [[NSAttributedString alloc] initWithString:line attributes:disasmBaseAttrs];
+		history[HISTORY_SIZE - 1] = [[NSAttributedString alloc] initWithString:line attributes:disasmBaseAttrs];
 	}
 	else if ( type == MONITOR_LOG_USER_INPUT )
 	{
-		history[historySize-1] = [[NSAttributedString alloc] initWithString:line attributes:labelAttrs];
+		history[HISTORY_SIZE - 1] = [[NSAttributedString alloc] initWithString:line attributes:labelAttrs];
 	}
 	else
 	{
-		history[historySize-1] = [[NSAttributedString alloc] initWithString:line attributes:valueAttrs];
+		history[HISTORY_SIZE - 1] = [[NSAttributedString alloc] initWithString:line attributes:valueAttrs];
 	}
 }
 
@@ -208,258 +216,13 @@ const int historySize = 1024;
 
 	[[NSColor blackColor] set];
 	NSRectFill(frame);
-
-	if ( halted )
-	{
-		NSPoint textPoint = frame.origin;
-		textPoint.x += 4;
-		textPoint.y = textPoint.y + frame.size.height - FONT_HEIGHT - 1;
-
-		[@"  R0" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R1" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R2" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R3" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R4" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R5" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R6" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R7" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R8" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  R9" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" R10" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" R11" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" R12" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" R13" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  LR" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  PC" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"CPSR" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"SPSR" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		// blank
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" Tmr" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" TM0" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" TM1" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" TM2" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" TM3" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" RTC" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" Alm" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  IR" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" ICR" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@"  FM" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" IC1" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" IC2" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[@" IC3" drawAtPoint:textPoint withAttributes:labelAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		// Disassembly
-
-		// blank
-		textPoint.y -= FONT_HEIGHT;
-
-		[symbol drawAtPoint:textPoint withAttributes:symbolAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		textPoint.x += FONT_WIDTH * 5;
-
-		for ( int i = 0; i < 5; ++i )
-		{
-			NSMutableAttributedString *tempStr = [[NSMutableAttributedString alloc] initWithAttributedString:disasm[i]];
-
-			if ( i == 0 )
-			{
-				[tempStr setAttributes:disasmBaseHighlightedAttrs range:NSMakeRange(0, [tempStr length])];
-			}
-			else
-			{
-				[tempStr setAttributes:disasmBaseAttrs range:NSMakeRange(0, [tempStr length])];
-			}
-
-			[tempStr setAttributes:disasmAddrAttrs range:NSMakeRange(0, 8)];
-
-			NSRange statusRange = [[tempStr string] rangeOfString:@"  ="];
-			if ( statusRange.location != NSNotFound )
-			{
-				statusRange.length = [tempStr length] - statusRange.location;
-				[tempStr setAttributes:symbolAttrs range:statusRange];
-			}
-
-			statusRange = [[tempStr string] rangeOfString:@"(will skip)"];
-			if ( statusRange.location != NSNotFound )
-				[tempStr setAttributes:disasmStatusAttrs range:statusRange];
-
-			statusRange = [[tempStr string] rangeOfString:@"(will do it)"];
-			if ( statusRange.location != NSNotFound )
-				[tempStr setAttributes:disasmStatusAttrs range:statusRange];
-
-			[tempStr drawAtPoint:textPoint];
-
-			textPoint.y -= FONT_HEIGHT;
-		}
-
-		// Register values
-
-		textPoint = frame.origin;
-		textPoint.x += 4 + (FONT_WIDTH * 5);
-		textPoint.y = textPoint.y + frame.size.height - FONT_HEIGHT - 1;
-
-		for ( int i = 0; i < 16; ++ i)
-		{
-			if ( regValChanged[i] )
-				[regVal[i] drawAtPoint:textPoint withAttributes:changedValueAttrs];
-			else
-				[regVal[i] drawAtPoint:textPoint withAttributes:valueAttrs];
-
-			textPoint.y -= FONT_HEIGHT;
-		}
-
-		[cpsr drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[spsr drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		// Skip down to Tmr
-		textPoint.y -= FONT_HEIGHT;
-
-		[frozenTimer drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		for ( int i = 0; i < 4; ++ i)
-		{
-			[tmRegVal[i] drawAtPoint:textPoint withAttributes:valueAttrs];
-			textPoint.y -= FONT_HEIGHT;
-		}
-
-		[rtc drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[alarm drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[intRaised drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[intCtrlReg drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[fm drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[ic1 drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[ic2 drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		[ic3 drawAtPoint:textPoint withAttributes:valueAttrs];
-		textPoint.y -= FONT_HEIGHT;
-
-		// History
-
-		textPoint = frame.origin;
-		textPoint.x += 4 + (FONT_WIDTH * 19);
-		textPoint.y = textPoint.y + frame.size.height - FONT_HEIGHT - 1;
-
-		for ( int i = 0; i < historyVisible; ++ i)
-		{
-			[history[i+(int)historyOffset] drawAtPoint:textPoint];
-			textPoint.y -= FONT_HEIGHT;
-		}
-
-		// Command line
-
-		textPoint = frame.origin;
-		textPoint.x += 4;
-		textPoint.y = textPoint.y + frame.size.height - (FONT_HEIGHT * 41) - 1;
-
-		[[NSString stringWithFormat:@"> %@_", command] drawAtPoint:textPoint withAttributes:labelAttrs];
-
-		// Separator lines
-
-		[[NSColor colorWithCalibratedRed:0.25 green:0.25 blue:0.25 alpha:1.0] set];
-
-		// Vertical, between registers and history
-
-		[NSBezierPath strokeLineFromPoint:NSMakePoint(FONT_WIDTH * 18, frame.size.height - (FONT_HEIGHT * 32.5)) toPoint:NSMakePoint(FONT_WIDTH * 18, frame.size.height)];
-
-		// Horizontal, between SPSR and Tmr
-
-		[NSBezierPath strokeLineFromPoint:NSMakePoint(0, frame.size.height - (FONT_HEIGHT * 18.5)) toPoint:NSMakePoint(FONT_WIDTH * 18, frame.size.height - (FONT_HEIGHT * 18.5))];
-
-		// Horizontal, between registers and disassembly
-
-		[NSBezierPath strokeLineFromPoint:NSMakePoint(0, frame.size.height - (FONT_HEIGHT * 32.5)) toPoint:NSMakePoint(frame.size.width, frame.size.height - (FONT_HEIGHT * 32.5))];
-
-		// Horizontal, between disassembly and input line
-
-		[NSBezierPath strokeLineFromPoint:NSMakePoint(0, frame.size.height - (FONT_HEIGHT * 39.5)) toPoint:NSMakePoint(frame.size.width, frame.size.height - (FONT_HEIGHT * 39.5))];
-	}
-	else
+	
+	if ( !halted )
 	{
 		NSPoint textPoint = frame.origin;
 		textPoint.x += 20;
 		textPoint.y += 20;
-
+		
 		if ( started )
 		{
 			[@"Emulator is running. Click Stop to stop it." drawAtPoint:textPoint withAttributes:labelAttrs];
@@ -468,7 +231,261 @@ const int historySize = 1024;
 		{
 			[@"Emulator has not yet started." drawAtPoint:textPoint withAttributes:labelAttrs];
 		}
+		
+		return;
 	}
+
+	NSPoint textPoint = frame.origin;
+	textPoint.x += 4;
+	textPoint.y = textPoint.y + frame.size.height - FONT_HEIGHT - 1;
+
+	[@"  R0" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R1" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R2" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R3" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R4" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R5" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R6" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R7" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R8" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  R9" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" R10" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" R11" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" R12" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" R13" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  LR" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  PC" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"CPSR" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"SPSR" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	// blank
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" Tmr" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" TM0" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" TM1" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" TM2" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" TM3" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" RTC" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" Alm" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  IR" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" ICR" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@"  FM" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" IC1" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" IC2" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[@" IC3" drawAtPoint:textPoint withAttributes:labelAttrs];
+	textPoint.y -= FONT_HEIGHT;
+	
+	// Register values
+
+	textPoint = frame.origin;
+	textPoint.x += 4 + (FONT_WIDTH * 5);
+	textPoint.y = textPoint.y + frame.size.height - FONT_HEIGHT - 1;
+
+	for ( int i = 0; i < 16; ++ i)
+	{
+		if ( regValChanged[i] )
+			[regVal[i] drawAtPoint:textPoint withAttributes:changedValueAttrs];
+		else
+			[regVal[i] drawAtPoint:textPoint withAttributes:valueAttrs];
+
+		textPoint.y -= FONT_HEIGHT;
+	}
+
+	[cpsr drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[spsr drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	// Skip down to Tmr
+	
+	textPoint.y -= FONT_HEIGHT;
+
+	[frozenTimer drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	for ( int i = 0; i < 4; ++ i)
+	{
+		[tmRegVal[i] drawAtPoint:textPoint withAttributes:valueAttrs];
+		textPoint.y -= FONT_HEIGHT;
+	}
+
+	[rtc drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[alarm drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[intRaised drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[intCtrlReg drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[fm drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[ic1 drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[ic2 drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+
+	[ic3 drawAtPoint:textPoint withAttributes:valueAttrs];
+	textPoint.y -= FONT_HEIGHT;
+	
+	// Current symbol
+	
+	textPoint = frame.origin;
+	textPoint.x = 4;
+	textPoint.y = (FONT_HEIGHT * 7);
+	
+	[symbol drawAtPoint:textPoint withAttributes:symbolAttrs];
+
+	// Disassembly context
+	
+	textPoint = frame.origin;
+	textPoint.x = 4 + (FONT_WIDTH * 21);
+	textPoint.y = (FONT_HEIGHT * 6);
+
+	for ( int i = 0; i < 5; ++i )
+	{
+		NSMutableAttributedString *tempStr = [[NSMutableAttributedString alloc] initWithAttributedString:disasm[i]];
+
+		if ( i == 0 )
+			[tempStr setAttributes:disasmBaseHighlightedAttrs range:NSMakeRange(0, [tempStr length])];
+		else
+			[tempStr setAttributes:disasmBaseAttrs range:NSMakeRange(0, [tempStr length])];
+
+		[tempStr setAttributes:disasmAddrAttrs range:NSMakeRange(0, 8)];
+
+		NSRange statusRange = [[tempStr string] rangeOfString:@"  ="];
+		if ( statusRange.location != NSNotFound )
+		{
+			statusRange.length = [tempStr length] - statusRange.location;
+			[tempStr setAttributes:symbolAttrs range:statusRange];
+		}
+
+		statusRange = [[tempStr string] rangeOfString:@"(will skip)"];
+		if ( statusRange.location != NSNotFound )
+			[tempStr setAttributes:disasmStatusAttrs range:statusRange];
+
+		statusRange = [[tempStr string] rangeOfString:@"(will do it)"];
+		if ( statusRange.location != NSNotFound )
+			[tempStr setAttributes:disasmStatusAttrs range:statusRange];
+
+		[tempStr drawAtPoint:textPoint];
+
+		textPoint.y -= FONT_HEIGHT;
+	}
+
+	// History
+
+	textPoint = frame.origin;
+	textPoint.x += 4 + (FONT_WIDTH * 19);
+	textPoint.y = textPoint.y + frame.size.height - FONT_HEIGHT - 1;
+
+	for ( int i = 0; i < historyVisible; ++ i)
+	{
+		[history[i + (int)historyOffset] drawAtPoint:textPoint];
+		textPoint.y -= FONT_HEIGHT;
+	}
+
+	// Command line
+
+	textPoint = frame.origin;
+	textPoint.x += 4;
+	textPoint.y = 4;
+
+	[[NSString stringWithFormat:@"> %@_", command] drawAtPoint:textPoint withAttributes:labelAttrs];
+
+	// Separator lines
+
+	[[NSColor colorWithCalibratedRed:0.25 green:0.25 blue:0.25 alpha:1.0] set];
+
+	NSInteger w = frame.size.width;
+	
+	// Vertical, between registers and history
+
+	[NSBezierPath strokeLineFromPoint:NSMakePoint(FONT_WIDTH * 18, (FONT_HEIGHT * 9) - 3) toPoint:NSMakePoint(FONT_WIDTH * 18, frame.size.height)];
+
+	// Horizontal, between SPSR and Tmr
+
+	NSInteger y = frame.size.height - (FONT_HEIGHT * 18.5);
+
+	[NSBezierPath strokeLineFromPoint:NSMakePoint(0, y) toPoint:NSMakePoint(FONT_WIDTH * 18, y)];
+
+	// Horizontal, between registers and disassembly
+
+	y = (FONT_HEIGHT * 9) - 4;
+	
+	[NSBezierPath strokeLineFromPoint:NSMakePoint(0, y) toPoint:NSMakePoint(w, y)];
+
+	// Horizontal, between disassembly and input line
+
+	y =	FONT_HEIGHT + 9;
+	
+	[NSBezierPath strokeLineFromPoint:NSMakePoint(0, y) toPoint:NSMakePoint(w, y)];
 }
 
 
@@ -505,21 +522,41 @@ const int historySize = 1024;
 	}
 }
 
+
+- (void)resizeWithOldSuperviewSize:(NSSize)oldSize
+{
+	[super resizeWithOldSuperviewSize:oldSize];
+	
+	NSRect frame = [self frame];
+	historyVisible = (frame.size.height - (FONT_HEIGHT * 9)) / FONT_HEIGHT;
+	historyOffset = (CGFloat)(HISTORY_SIZE - historyVisible);
+
+	[self setNeedsDisplay:YES];
+}
+
 - (void)scrollWheel:(NSEvent *)event
 {
 	if ( !started || !halted )
 		return;
 
 	CGFloat dy = [event scrollingDeltaY];
-	if ([event hasPreciseScrollingDeltas])
+
+	if ( [event hasPreciseScrollingDeltas] )
 		dy /= FONT_HEIGHT;
-	if (dy!=0.0) {
+	
+	if ( dy != 0.0 )
+	{
 		CGFloat prevOffset = historyOffset;
 		historyOffset -= dy;
-		if (historyOffset<0.0) historyOffset = 0.0;
-		if (historyOffset>(int)(historySize-historyVisible))
-			historyOffset = (CGFloat)(historySize-historyVisible);
-		if (prevOffset!=historyOffset) [self setNeedsDisplay:YES];
+		
+		if ( historyOffset < 0.0 )
+			historyOffset = 0.0;
+		
+		if ( historyOffset > (int)(HISTORY_SIZE - historyVisible) )
+			historyOffset = (CGFloat)(HISTORY_SIZE - historyVisible);
+		
+		if ( prevOffset != historyOffset)
+			[self setNeedsDisplay:YES];
 	}
 }
 
