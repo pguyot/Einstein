4f819a78df944b5d3c31f5b640a7b5690d2780d1
Matthias Melcher <git@matthiasm.com>
2018-04-27 17:51:59 +0200

    Added missing files for Serial Emulation

---
+++ Emulator/Serial/TVoyagerManagedSerialPortNamedPipes.h
@@ -0,0 +1,109 @@
+// ==============================
+// File:			TVoyagerManagedSerialPortNamedPipes.h
+// Project:			Einstein
+//
+// Copyright 2018 by Matthias Melcher (mm@matthiasm.com).
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// ==============================
+// $Id$
+// ==============================
+
+#ifndef _TVOYAGERMANAGEDSERIALPORTNAMEDPIPES_H
+#define _TVOYAGERMANAGEDSERIALPORTNAMEDPIPES_H
+
+#include "TVoyagerManagedSerialPort.h"
+
+#include <pthread.h>
+
+class TLog;
+class TInterruptManager;
+class TDMAManager;
+class TMemory;
+
+///
+/// Emulate a serial port via named pipes in MacOS
+///
+/// \author Matthias Melcher
+///
+class TVoyagerManagedSerialPortNamedPipes : public TVoyagerManagedSerialPort
+{
+public:
+
+	///
+	/// Constructor.
+	///
+	TVoyagerManagedSerialPortNamedPipes(
+							  TLog* inLog,
+							  ELocationID inLocationID,
+							  TInterruptManager* inInterruptManager,
+							  TDMAManager* inDMAManager,
+							  TMemory* inMemory);
+
+	///
+	/// Destructor.
+	///
+	virtual ~TVoyagerManagedSerialPortNamedPipes( void );
+
+	///
+	/// DMA or interrupts trigger a command that must be handled by a derived class.
+	///
+	virtual void TriggerEvent(KUInt8 cmd);
+
+protected:
+
+	///
+	/// Launch the thread that emulates the DMA hardware
+	///
+	void RunDMA();
+
+	///
+	/// Emulate the DMA hardware
+	///
+	void HandleDMA();
+
+	///
+	/// PThread hook.
+	///
+	static void *SHandleDMA(void *This) { ((TVoyagerManagedSerialPortNamedPipes*)This)->HandleDMA(); return 0L; }
+
+	///
+	/// Find good names for the named pipes
+	///
+	void FindPipeNames();
+
+	///
+	/// Create the named pipes as nodes in the file system
+	///
+	bool CreateNamedPipes();
+
+	int mPipe[2];							///< communication between emulator and DMA thread
+	int mTxPort;							///< named pipe or serial port
+	int mRxPort;							///< named pipe or serial port
+	int mNamedSendPipe;						///< serial port emulation ("/Users/matt/Library/Application Support/Einstein Emulator/ExtrSerPortSend")
+	int mNamedRecvPipe;						///< serial port emulation ("/Users/matt/Library/Application Support/Einstein Emulator/ExtrSerPortRecv")
+	bool mDMAIsRunning;						///< set if DMA thread is active
+	pthread_t mDMAThread;
+
+	char *mTxPortName;						///< named pipe for transmitting data
+	char *mRxPortName;						///< named pipe for receiving data
+};
+
+#endif
+// _TVOYAGERMANAGEDSERIALPORTNAMEDPIPES_H
+
+// ================= //
+// Byte your tongue. //
+// ================= //
