0c52b790eaf27e8ff0470ad4e736c034ed9dced4
Matthias Melcher <git@matthiasm.com>
2019-03-29 21:47:12 +0100

    Very basic setup for the Proteus Analyzer and ROM patcher

---
+++ Proteus/proteus.cpp
@@ -0,0 +1,63 @@
+// ==============================
+// File:			proteus.h
+// Project:			Einstein
+//
+// Copyright 2019 by Matthais Melcher (proteus@matthiasm.com).
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// ==============================
+// $Id$
+// ==============================
+
+
+#include "proteus.h"
+
+// --- general includes
+
+
+// --- macros
+
+
+// --- class predeclarations
+
+
+// --- typedefs
+
+
+// --- classes
+
+
+// --- globals
+
+// /* 0x0c008400-0x0c107e18 */
+
+// --- ROM
+
+// /* 0x00000000-0x0071FC4C */
+
+// --- RExBlock
+
+// /* 0x0071FC4C-0x007EE048 */
+
+// --- Jump Table (not used here)
+
+//-/* 0x01a00000-0x01c1085C */
+
+// ============================================================================== //
+// I've ended up calling my cat Brexit. It wakes me up meowing like crazy every
+// morning because it wants to go out, but as soon as I open the door, it just
+// sits there undecided and then looks angry when I put it outside.
+//   -- French Europe Minister Nathalie Loiseau
+// ============================================================================== //
---
+++ _Build_/Xcode/Tools/Squash/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  squash
+//
+//  Created by Matthias Melcher on 3/29/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
