2ac4f21b379aaa83b2465316a8fe08a842c2800a
Matthias Melcher <git@matthiasm.com>
2019-04-02 03:00:08 +0200

    Working SWI code until function dispatch. Ugly, but working.

--- Proteus/Proteus.cpp
+++ Proteus/Proteus.cpp
@@ -59,11 +59,14 @@ TMemory *MEM = nullptr;
 #define R15 CPU->mCurrentRegisters[15]
 #define PC CPU->mCurrentRegisters[15]
 
+/** Exit the native code to this address in the interpreter */
+#define EXIT(addr) { R15 = addr+4; return nullptr; }
+
 /** Push a value onto the stack (stmdb) */
-#define PUSH(sp, val) { sp-=4; MEM->Write(sp, val); } 
+#define PUSH(sp, w) { sp-=4; MEM->Write(sp, w); } 
 
 /** Pop a value from the stack (ldmia) */
-inline KUInt32 POP(KUInt32 &sp) { KUInt32 w; MEM->Read(sp, w); sp+=4; } 
+#define POP(sp, w) { MEM->Read(sp, w); sp+=4; } 
 
 /** Read a word from memory */
 inline KUInt32 PEEK_W(KUInt32 addr) { KUInt32 w; MEM->Read(addr, w); return w; }
@@ -88,6 +91,8 @@ inline KUInt32 PEEK_W(KUInt32 addr) { KUInt32 w; MEM->Read(addr, w); return w; }
 
 
 // --- function declaraitions
+
+JITUnit *SWI_Scheduler();
 
 /** TODO: write me */
 void DoDeferrals();
@@ -154,6 +159,16 @@ T_ROM_INJECTION(0x00000000, "Initialize Proteus") {
 // TODO: implememt this to finish SWIs
 // /* 0x00148298-0x003AD698 */
 
+/* movs pc, lr
+T_ROM_INJECTION(0x003ADA6C, "movs")
+{
+	PC = LR+4;
+	CPU->SetCPSR( CPU->GetSPSR() );
+	return nullptr;
+}
+*/
+
+
 /**
  * Handle all operations that need to be executet in supervisor mode.
  *
@@ -167,71 +182,144 @@ T_ROM_INJECTION(0x00000000, "Initialize Proteus") {
  */
 T_ROM_INJECTION(0x003AD698, "SWIBoot")
 {
+// /* 0x003AD698-0x003AD69C */
     PUSH(SP, R1);
     PUSH(SP, R0);
-    KUInt32 cmd = PEEK_W(LR-4);
-    // Check if we were called through the SWI instruction
-    if ( (cmd&0x0F000000) != 0x0F000000 ) {
-        // FIXME:
-        R0 = cmd;
-        R1 = cmd & 0x0F000000;
-        R15 = 0x003ADD70+4;
-        return nullptr;
-        //goto L003ADD70; // FIXME:
-    }
-    // Check if this was a conditional call
+// /* 0x003AD69C-0x003AD6A0 */
+    R0 = PEEK_W(LR-4);
+// /* 0x003AD6A0-0x003AD6A4 */
+    R1 = R0 & 0x0F000000;
+// /* 0x003AD6A4-0x003AD6AC */
+    if (R1!=0x0F000000) {
+		POP(SP, R0);
+		POP(SP, R1);
+		LR -= 4;
+		PC = LR+4;
+		CPU->SetCPSR( CPU->GetSPSR() );
+		return nullptr;
+	}
+L003AD6AC:
 // /* 0x003AD6AC-0x003AD6B0 */
-    if ( (cmd&0xF0000000) != 0xE0000000 ) {
-        // FIXME:
-        R0 = cmd;
-        R1 = cmd & 0xF0000000;
-        R15 = 0x003ADD7C+4;
-        return nullptr;
-        //goto L003ADD7C; // FIXME:
-    }
+    R1 = R0 & 0xF0000000;
+// /* 0x003AD6B0-0x003AD6B8 */
+    if (R1!=0xE0000000) {
+		bool cond = false;
+		R0 = CPU->GetSPSR();
+		switch (R1>>24) {
+			case 0x00: cond = (R0!=0x40000000); break; // EQ
+			case 0x01: cond = (R0==0x40000000); break; // NE
+			case 0x02: cond = (R0!=0x20000000); break; // CS
+			case 0x03: cond = (R0==0x20000000); break; // CC
+			case 0x04: cond = (R0!=0x80000000); break; // MI
+			case 0x05: cond = (R0==0x80000000); break; // PL
+			case 0x06: cond = (R0!=0x10000000); break; // VS
+			case 0x07: cond = (R0==0x10000000); break; // VC
+			case 0x08: cond = ((R0&~(R0>>1))!=0x20000000); break; // HI
+			case 0x09: cond = ((R0&~(R0>>1))==0x20000000); break; // LS
+			case 0x0A: cond = ((R0^(R0<<3))!=0x80000000); break; // GE
+			case 0x0B: cond = ((R0^(R0<<3))==0x80000000); break; // LT
+			case 0x0C: cond = (((R0^(R0<<3))|(R0<<1))!=0x80000000); break; // GT
+			case 0x0D: cond = (((R0^(R0<<3))|(R0<<1))==0x80000000); break; // LE
+			case 0x0E: cond = true; break; // AL
+			case 0x0F: cond = false; break; // NV
+		}
+		if (cond==false) {
+			POP(SP, R0);
+			POP(SP, R1);
+			LR -= 4;
+			PC = LR+4;
+			CPU->SetCPSR( CPU->GetSPSR() );
+			return nullptr;
+		}
+	} 
+L003AD6B8:
 // /* 0x003AD6B8-0x003AD6BC */
     R1 = CPU->GetSPSR();
-// /* 0x003AD6B8-0x003AD6BC */
-    R1 = R1 & 0x1f;
-// /* 0x003AD6BC-0x003AD6CC */
-    if (R1!=0x10 && R1!=0x00)
-        throw "SWI from non-user mode (rebooting)";
-    // FIXME:
-    R0 = cmd;
-    R15 = 0x003AD6CC+4;
-    return nullptr;
-}
-// /* 0x003AD6CC-0x003AD740 */
+// /* 0x003AD6BC-0x003AD6C0 */
+    R1 = R1 & 0x1F; // get the CPU mode before calling the SWI
+// /* 0x003AD6C0-0x003AD6CC */
+    if (R1!=0x10 && R1!=0)
+		throw "SWI from non-user mode (rebooting)";
+L003AD6CC:
+// /* 0x003AD6CC-0x003AD6D0 */
+    R1 = 0x55555555;
+// /* 0x003AD6D0-0x003AD6E0 */
+    PUSH(SP, R1);
+    PUSH(SP, R0);
+// /* 0x003AD6E0-0x003AD6E4 */
+    R0 = R1;
+// /* 0x003AD6E4-0x003AD6E8 */
+    R1 = 0x0C008400;
+// /* 0x003AD6E8-0x003AD6EC */
+    POKE_W(R1+176, R0); // current domain access control setting (gParamBlockFromImage+176)
+// /* 0x003AD6EC-0x003AD6F0 */
+    POP(SP, R0);
+    POP(SP, R1);
+// /* 0x003AD6F0-0x003AD6F4 */
+    MEM->SetDomainAccessControl(R1);
+// /* 0x003AD6F4-0x003AD6FC */
+    if (   GetGAtomicFIQNestCountFast()!=0
+        || GetGAtomicIRQNestCountFast()!=0
+        || GetGAtomicNestCount()!=0
+        || GetGAtomicFIQNestCount()!=0 )
+        goto L003AD734;
+// /* 0x003AD6FC-0x003AD72C */
+    // bit7=IRQ, bit6=FIQ, bit0-4=mode (10=usr, 11=FIQ, 12=IRQ, 13=abt, 17=svc, 1b=und, 1f=system)
+    CPU->SetCPSR( (CPU->GetCPSR()&~0xff) | 0x13 ); // Enable all interrupts
+// /* 0x003AD72C-0x003AD738 */
+L003AD734:
+    R1 = LR;
+// /* 0x003AD738-0x003AD73C */
+    R1 = PEEK_W(R1-4);
+// /* 0x003AD73C-0x003AD740 */
+    R1 = R1 & 0x00FFFFFF;
     // r1 contains the ID of the desired swi function
 // /* 0x003AD740-0x003AD744 */
     // if the ID is out of range, the OS throws a System Panic. 
-// /* 0x003AD744-0x003AD74C */
+// /* 0x003AD744-0x003AD748 */
+    if (R1>=35)
+		throw "Undefined SWI";
+// /* 0x003AD748-0x003AD74C */
+    R0 = 0x003AD56C; // address of jump table
     // use the function id to index into a large jump table at 0x003AD56C
-// /* 0x003AD748-0x003AD750 */
-    // All functions called in the dispatch above will eventually end here, in the scheduler.
-    // The scheduler will check if it is time to run another thread in the multithreaded 
-    // system. Thread changes are usually requested by external events or by the timer.
-    // This is called preemptive multitasking. If a task does not call any SWIs for a long 
-    // time, all other tasks in teh system are blocked and won;t run.
-    //
-    // NewtonOS has one very special event that can (and must) trigger a task change: if a
-    // user task reads or writes an address that was marked by the Memory Managemnet Unit (MMU),
-    // an interrupt is called that may allocate more memory to the location that was read.
-    // Since allocating memory may trigger major reorganizetions, Newt puts the current
-    // task to sleep and switches to teh memory management task. Only if that task is succesful,
-    // the original task will be rescheduled to try the same memory access again.
-    //
-    // Until memory management and task switching is fully implemented, there is no use in
-    // implementing any user code. Otherwies, user code could trigger a memory exeception
-    // at any time and crash teh system if not handled correctly.
-    //
-    // Start of SWI Scheduler
-T_ROM_INJECTION(0x003AD750, "SWI_Scheduler")
+
+	switch (R1) {
+		case 34: // L003AE14C
+			POP(SP, R0);
+			POP(SP, R1);
+			return SWI_Scheduler();
+		default:
+			EXIT(0x003AD74C);
+    }
+// /* 0x003AD74C-0x003AD750 */
+}
+
+
+T_ROM_INJECTION(0x003AD750, "SWI_Scheduler") { return SWI_Scheduler(); }
+/**
+ * This function is called at the end of SWIs.
+ *
+ * All functions called in the dispatch above will eventually end here, in the scheduler.
+ * The scheduler will check if it is time to run another thread in the multithreaded 
+ * system. Thread changes are usually requested by external events or by the timer.
+ * This is called preemptive multitasking. If a task does not call any SWIs for a long 
+ * time, all other tasks in teh system are blocked and won;t run.
+ *
+ * NewtonOS has one very special event that can (and must) trigger a task change: if a
+ * user task reads or writes an address that was marked by the Memory Managemnet Unit (MMU),
+ * an interrupt is called that may allocate more memory to the location that was read.
+ * Since allocating memory may trigger major reorganizetions, Newt puts the current
+ * task to sleep and switches to teh memory management task. Only if that task is succesful,
+ * the original task will be rescheduled to try the same memory access again.
+ *
+ * Until memory management and task switching is fully implemented, there is no use in
+ * implementing any user code. Otherwies, user code could trigger a memory exeception
+ * at any time and crash teh system if not handled correctly.
+ */
+JITUnit *SWI_Scheduler()
 {
-L003AD750:
     PUSH(SP, R1);
     PUSH(SP, R0);
-
     // bit7=IRQ, bit6=FIQ, bit0-4=mode (10=usr, 11=FIQ, 12=IRQ, 13=abt, 17=svc, 1b=und, 1f=system)
     CPU->SetCPSR( (CPU->GetCPSR()&~0xff) | 0xD3 ); // Disable interrupts, abt mode
     if (   GetGAtomicFIQNestCountFast()!=0
