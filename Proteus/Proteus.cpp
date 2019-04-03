f157155fade7b9a4377ec7ddec1cd971ce71d01b
Matthias Melcher <git@matthiasm.com>
2019-04-03 12:08:43 +0200

    Added a dependency tree for the scheduler.
    The sheer size and depth of the tree is very discouraging. I will have
    to reinstate using fibers to pop in and out of native code.

--- Proteus/Proteus.cpp
+++ Proteus/Proteus.cpp
@@ -70,6 +70,9 @@ TInterruptManager *INT = nullptr;
 /** Pop a value from the stack (ldmia) */
 #define POP(sp, w) { MEM->Read(sp, w); sp+=4; } 
 
+/** Reverse pop, (ldmdb) */
+#define RPOP(sp, w) { sp-=4; MEM->Read(sp, w); } 
+
 /** Read a word from memory */
 inline KUInt32 PEEK_W(KUInt32 addr) { KUInt32 w; MEM->Read(addr, w); return w; }
 
@@ -90,14 +93,20 @@ inline KUInt32 PEEK_W(KUInt32 addr) { KUInt32 w; MEM->Read(addr, w); return w; }
 #define	T_GETSET_MEMVAR_W(offset, type, name) \
 	type Get##name() { KUInt32 w; MEM->Read(((KUInt32)(uintptr_t)this)+offset, w); return (type)w; } \
 	void Set##name(type w) { KUInt32 v = (KUInt32)w; MEM->Write(((KUInt32)(uintptr_t)this)+offset, v); }
+#define	T_GETSET_MEMVAR_P(offset, type, name) \
+	type Get##name() { KUInt32 w; MEM->Read(((KUInt32)(uintptr_t)this)+offset, w); return (type)(uintptr_t)w; } \
+	void Set##name(type w) { KUInt32 v = (KUInt32)(uintptr_t)w; MEM->Write(((KUInt32)(uintptr_t)this)+offset, v); }
 
 // --- class predeclarations
 
 class ImageParamBlock;
-
+class TTaskQueue;
+class TTask;
+class TTaskContainer;
 
 // --- typedefs
 
+typedef KUInt32 KernelObjectState;
 
 // --- function declaraitions
 
@@ -113,18 +122,132 @@ JITUnit *_SWIScheduler();
 /** Make sure that the following commands are run uninterrupted. */
 void _EnterAtomicFast();
 
+/** End the atomic operation started by _EnterAtomicFast(). */
+void _ExitAtomicFast();
+
+/** Call the SWI that allows the OS to change to a different task. */
+void DoSchedulerSWI();
+
 /** TODO: write me */
 void DoDeferrals();
 // Dependency tree:
-//   _EnterAtomicFast
-//   _ExitAtomicFast
-//     DoSchedulerSWI (loops!)
-//   PortDeferredSendNotify__Fv
-//     (many?)
-//   DeferredNotify__Fv
-//     (many?)
-//   DoDeferral__18TExtPageTrackerMgrFv
-//     (many?)
+//	_EnterFIQAtomicFast (OK)
+//	_ExitAtomicFast
+//		DoSchedulerSWI (loops!)
+//	PortDeferredSendNotify__Fv
+//		Remove__17TDoubleQContainerFv (end)
+//		QueueDelay__12TTimerEngineFP13TSharedMemMsg
+//			CompCompare (end)
+//			Queue__12TTimerEngineFP13TSharedMemMsg
+//				Peek__17TDoubleQContainerFv (end)
+//				SetAlarmAtomic__FR5TTime
+//					SetAlarm
+//						SetAlarm1 (end)
+//						DisableAlarm1 (end)
+//				Alarm__12TTimerEngineFv
+//					Peek__17TDoubleQContainerFv (above)
+//					ClearAlarmAtomic__Fv
+//						_EnterFIQAtomicFast (end)
+//						ClearAlarm
+//							DisableAlarm1 (end)
+//						_ExitFIQAtomicFast (DoSchedulerSWI)
+//					GetClock (end)
+//					CompCompare (end)
+//					Remove__17TDoubleQContainerFv (end)
+//					SetAlarmAtomic__FR5TTime (above)
+//				AddToFront__17TDoubleQContainerFPv
+//					CheckBeforeAdd__17TDoubleQContainerFPv (end)
+//				AddBefore__17TDoubleQContainerFPvT1
+//					CheckBeforeAdd__17TDoubleQContainerFPv (end)
+//					AddToFront__17TDoubleQContainerFPv (above)
+//				GetNext__17TDoubleQContainerFPv (end)
+//				Add__17TDoubleQContainerFPv
+//					CheckBeforeAdd__17TDoubleQContainerFPv (end)
+//		NotifySend__FP13TSharedMemMsg
+//			ConvertIdToObj__F11KernelTypesUlPv
+//				LocalToGlobalId__FUl ?
+//				Get__12TObjectTableFUl
+//					Get__12TObjectTableFUl (end)
+//			Send__5TPortFP13TSharedMemMsgUl
+//				QueueTimeout__12TTimerEngineFP13TSharedMemMsg
+//					CompAdd (end)
+//					__nw__FUi
+//						malloc
+//							NewPtr 
+//								ReportMemMgrTrap__Fl
+//									sprintf
+//										memset (end)
+//										__vfprintf ?
+//										putc
+//											__flsbuf ?
+//												...
+//												_writebuf ?
+//													_sys_flen ?
+//													_sys_seek ?
+//													_sys_write ?
+//														Unnamed_00319030 ?
+//														TapFileCntl (ARM: 0xE6000810)
+//								HashCallChain (end)
+//								GetHeap
+//									GetCurrentHeap (end)
+//								IsSafeHeap__FPv (end)
+//								GetHeapSemaphore__FPv
+//									GetSkiaHeapSemaphore
+//										GetCurrentHeap (above)
+//								Acquire__18TULockingSemaphoreF8SemFlags
+//									SemOp__16TUSemaphoreGroupFP17TUSemaphoreOpList8SemFlags
+//										swi(11) -> DoSemaphoreOp
+//											SemOp__15TSemaphoreGroupFP16TSemaphoreOpList8SemFlagsP5TTask
+//												... Add__10TTaskQueueFP5TTask17KernelObjectStateP14TTaskContainer (end)
+//								CheckHeap
+//									AddExceptionHandler ?
+//									VetHeap ?
+//									Subexception ?
+//									NextHandler ?
+//									ExitHandler ?
+//								ReportSmashedHeap__FPclPv ?
+//								NewDirectBlock
+//									GetCurrentHeap (above)
+//									NewBlockLow
+//										NewBlock
+//											SearchFreeList
+//												FindSmallestSlide
+//													LockedBlock (end)
+//													SlideBlocksUp
+//														MoveBytes -> BlockMove -> memmove (end)
+//														SetFreeChain (end)
+//													GetCurrentHeap <----------
+//													SlideBlocksDown (above, SlideBlocksUp)
+//											ExtendVMHeap
+//												SetHeapLimits -> _MonitorDispatchSWI -> MonitorDispatchKernelGlue
+//													-> Aquire__8TMonitorFv 
+//												LockHeapRange -> _MonitorDispatchSWI
+//											MoveFreeBlock -> SetFreeChain
+//											RemoveFreeBlock (end)
+//								IncrementBlockBusy (end)
+//								Release__18TULockingSemaphoreFv
+//									SemOp__16TUSemaphoreGroupFP17TUSemaphoreOpList8SemFlag (above)
+//								SetPtrName (end)
+//					_EnterAtomicFast (ok)
+//					Queue__12TTimerEngineFP13TSharedMemMsg ?
+//					_ExitAtomicFast (above)
+//				Peek__17TDoubleQContainerFv (above)
+//				GetNext__17TDoubleQContainerFPv (above)
+//				UnScheduleTask__FP5TTask ?
+//				WantSchedule__Fv ?
+//				RemoveFromQueue__17TDoubleQContainerFPv ?
+//				CompleteReceiver__13TSharedMemMsgFP13TSharedMemMsgl ?
+//				AddToFront__17TDoubleQContainerFPv (above)
+//				Add__17TDoubleQContainerFPv (above)
+//			CompleteSender__13TSharedMemMsgFl ?
+//	DeferredNotify__Fv
+//		Remove__17TDoubleQContainerFv ?
+//		NotifyTimeout__FP13TSharedMemMsg ?
+//		NotifySend__FP13TSharedMemMsg (above)
+//	DoDeferral__18TExtPageTrackerMgrFv
+//		Peek__17TDoubleQContainerFv (above)
+//		DoDeferral__15TExtPageTrackerFv ?
+//		GetNext__17TDoubleQContainerFPv ?
 
 // --- classes
 
@@ -132,6 +255,27 @@ class ImageParamBlock
 {
 public:
 	T_GETSET_MEMVAR_W(176, KUInt32, CurrentDomainAccessControl);
+};
+
+class TTaskQueue
+{
+public:
+	void CheckBeforeAdd(TTask*);
+    void Add(TTask *aTask, KernelObjectState aState, TTaskContainer *aContainer);
+public:
+	T_GETSET_MEMVAR_P(0, TTask*, Task);
+	T_GETSET_MEMVAR_P(4, KUInt32, Unknown4);
+};
+
+class TTask 
+{
+public:
+	T_GETSET_MEMVAR_W(148, KUInt32, Unknown148);
+};
+
+class TTaskContainer
+{
+public:
 };
 
 // --- globals
@@ -187,17 +331,77 @@ T_ROM_INJECTION(0x00000000, "Initialize Proteus") {
 
 // /* 0x00000000-0x0014829C */
 // TODO: implememt this to finish SWIs
-// /* 0x00148298-0x00392AC0 */
+// /* 0x00148298-0x00359AA8 */
 
-
-T_ROM_INJECTION(0x00392AC0, "_EnterAtomicFast") {
-	_EnterAtomicFast();
+void TTaskQueue::CheckBeforeAdd(TTask *)
+{
+	return;
+}
+T_ROM_INJECTION(0x00359AA8, "CheckBeforeAdd__10TTaskQueueFP5TTask")
+{
+	((TTaskQueue*)(uintptr_t)(R0))->CheckBeforeAdd(((TTask*)(uintptr_t)(R1)));
 	EXIT(LR);
 }
 
+
+void TTaskQueue::Add(TTask *aTask, KernelObjectState aState, TTaskContainer *aContainer)
+{
+	R0 = (KUInt32)(uintptr_t)this;
+	R1 = (KUInt32)(uintptr_t)aTask;
+	R2 = (KUInt32)(uintptr_t)aState;
+	R3 = (KUInt32)(uintptr_t)aContainer;
+	R12 = SP;
+	PUSH(SP, PC);
+	PUSH(SP, LR);
+	PUSH(SP, R12);
+	PUSH(SP, R11);
+	PUSH(SP, R7);
+	PUSH(SP, R6);
+	PUSH(SP, R5);
+	PUSH(SP, R4);
+	R11 = R12 - 4;
+	R5 = R0;
+	R4 = R1;
+	R7 = R2;
+	R6 = R3;
+	aTask->SetUnknown148(0);
+	CheckBeforeAdd(aTask);
+	if (GetTask()==nullptr) {
+		SetTask(aTask);
+		POKE_W(R4+152, R5);
+	} else {
+		R0 = PEEK_W(R5+4);
+		R0 += 148; POKE_W(R0, R4);
+		R0 = PEEK_W(R5+4);
+		POKE_W(R4+152, R0);
+	}
+	SetUnknown4(R4);
+	R0 = PEEK_W(R4+108);
+	R0 = R0 | R7;
+	R4 += 108; POKE_W(R4, R0);
+	R4 += 36; POKE_W(R4, R6);
+	KUInt32 sp = R11;
+	RPOP(sp, PC);
+	RPOP(sp, SP);
+	RPOP(sp, R11);
+	RPOP(sp, R7);
+	RPOP(sp, R6);
+	RPOP(sp, R5);
+	RPOP(sp, R4);
+}
+T_ROM_INJECTION(0x00359AAC, "Add__10TTaskQueueFP5TTask17KernelObjectStateP14TTaskContainer")
+{
+    ((TTaskQueue*)(uintptr_t)(R0))->Add(((TTask*)(uintptr_t)(R1)), R2, ((TTaskContainer*)(uintptr_t)(R3)));
+    EXIT(PC);
+}
+
+
+// /* 0x00359B14-0x00392AC0 */
+
+
 /**
  * Start an atomic operation, asequence of commands that will not be iterrupted.
- * End the atomic operation by calling _LeaveAtomicFast().
+ * End the atomic operation by calling _ExitAtomicFast().
  */
 void _EnterAtomicFast() 
 {
@@ -211,8 +415,35 @@ void _EnterAtomicFast()
 	}
 }
 
+T_ROM_INJECTION(0x00392AC0, "_EnterAtomicFast") {
+	_EnterAtomicFast();
+	EXIT(LR);
+}
 
-// /* 0x00392B1C-0x003AD698 */
+
+
+void _ExitAtomicFast() 
+{
+// /* 0x00392B1C-0x00392B90 */
+}
+
+
+// /* 0x00392B90-0x003AD658 */
+
+
+// as long as the SWI returns directly to the emulator, we can't activate this call
+//T_ROM_INJECTION(0x003AD658, "DoSchedulerSWI") {
+//	DoSchedulerSWI();
+//	EXIT(LR);
+//}
+
+void DoSchedulerSWI()
+{
+	SWI(34);
+}
+
+
+// /* 0x003AD660-0x003AD698 */
 
 /**
  * This is the native entry point for the swi call.
