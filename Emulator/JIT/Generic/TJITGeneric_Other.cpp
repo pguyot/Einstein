61930fbc5d263a1e606760bde421102330a0df9f
Matthias Melcher <git@matthiasm.com>
2020-05-14 21:24:10 +0200

    Fix a bug where translating native code from packages may crash the emulator

--- Emulator/JIT/Generic/TJITGeneric_Other.cpp
+++ Emulator/JIT/Generic/TJITGeneric_Other.cpp
@@ -165,10 +165,15 @@ Translate_PatchNativeCall(
 					KUInt32 inInstruction,
 					KUInt32 inVAddr )
 {
-	KUInt32 ix = TJITGenericPatchObject::GetIndex(inInstruction);
-	PUSHFUNC(CallPatchNative);
-	PUSHVALUE(ix);
-	return TJITGenericPatchManager::GetPatchAt(ix)->GetOriginalInstruction();
+    KUInt32 ix = TJITGenericPatchObject::GetIndex(inInstruction);
+    TJITGenericPatchObject* patchObject = TJITGenericPatchManager::GetPatchAt(ix);
+    if (patchObject == NULL) {
+        // This patch doesn't exist, we're probably trying to translate data.
+        return inInstruction;
+    }
+    PUSHFUNC(CallPatchNative);
+    PUSHVALUE(ix);
+    return patchObject->GetOriginalInstruction();
 }
 
 
