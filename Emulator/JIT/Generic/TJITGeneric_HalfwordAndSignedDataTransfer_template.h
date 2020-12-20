9533078a0871f313cd5d91529287a9f056c0b124
MatthiasWM <git@matthiasm.com>
2020-12-20 23:18:33 +0100

    Fixed a typo in the JIT emulator.

--- Emulator/JIT/Generic/TJITGeneric_HalfwordAndSignedDataTransfer_template.h
+++ Emulator/JIT/Generic/TJITGeneric_HalfwordAndSignedDataTransfer_template.h
@@ -140,7 +140,7 @@ HalfwordAndSignedDataTransferReg_Template(BITS_FLAGS, Rn, Rd)
 
     // The L bit should not be set low (Store) when Signed (S=1) operations have been selected
     // Unsigned half-word
-    if (theMemoryInterface->WriteB( theAddress, (KUInt8) ((theValue > 8) & 0xFF) ))
+    if (theMemoryInterface->WriteB( theAddress, (KUInt8) ((theValue >> 8) & 0xFF) ))
     {
         SETPC(GETPC());
         ioCPU->DataAbort();
