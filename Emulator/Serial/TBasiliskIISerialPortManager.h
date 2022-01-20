f1afb7cd09706ebc3746f6bfddb3ae072559dd1f
Eckhart Köppen <eck@40hz.org>
2022-01-20 08:10:08 +0200

    Fix driver ID types

--- Emulator/Serial/TBasiliskIISerialPortManager.h
+++ Emulator/Serial/TBasiliskIISerialPortManager.h
@@ -71,7 +71,7 @@ public:
 	///
 	/// Return the Identification of this driver
 	///
-	KUInt32
+	TSerialPorts::EDriverID
 	GetID() override
 	{
 		return TSerialPorts::kBasiliskIIDriver;
--- Emulator/Serial/TTcpClientSerialPortManager.h
+++ Emulator/Serial/TTcpClientSerialPortManager.h
@@ -59,7 +59,7 @@ public:
 	///
 	/// Return the Identification of this driver
 	///
-	KUInt32
+	TSerialPorts::EDriverID
 	GetID() override
 	{
 		return TSerialPorts::kTcpClientDriver;
